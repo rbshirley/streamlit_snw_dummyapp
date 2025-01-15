1e26aaec01e27ce4fd4b1042bfa3b322e1dd37fe
rbshirley <shirler@ccf.org>
2025-01-15 14:42:44 -0500

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+# Everything is accessible via the st.secrets dict:
+
+st.write("DB username:", st.secrets["db_username"])
+st.write("DB password:", st.secrets["db_password"])
+
